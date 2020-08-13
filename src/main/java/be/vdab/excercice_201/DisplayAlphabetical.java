08307553ad4d8f251272e131e28e64b9c6a51beb
Thomas Goos <goosthomas@gmail.com>
2020-08-13 11:34:41 +0200

    What i've got so far

--- src/main/java/be/vdab/excercice_201/DisplayAlphabetical.java
+++ src/main/java/be/vdab/excercice_201/DisplayAlphabetical.java
@@ -1,48 +1,33 @@
 package be.vdab.excercice_201;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class DisplayAlphabetical {
     public static void main(String[] args) throws Exception {
-        // Check number of arguments passed
-        if (args.length != 1) {
-            System.out.println("Usage: java Exercise_20_01 TextFile");
-            System.exit(1);
-        }
-
-
-        // Check if file exists
-        File textFile = new File(args[0]);
-        if (!textFile.exists()) {
-            System.out.println("The file " + args[0] + " does not exist.");
-            System.exit(2);
-        }
-
-        // Create a list of string
         List<String> list = new ArrayList<>();
 
-        try (
-                // Create input file
-                Scanner input = new Scanner(textFile);
-        ) {
-            while (input.hasNext()) {
-                String[] array = input.nextLine().split(" ");
-                for (int i = 0; i < array.length; i++) {
-                    if (array[i].length() > 0 &&
-                            Character.isLetter(array[i].charAt(0))) {
-                        list.add(array[i]);
-                    }
-                }
+        Scanner sc2 = null;
+        try {
+            sc2 = new Scanner(new File("input.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (sc2.hasNextLine()) {
+            Scanner s2 = new Scanner(sc2.nextLine());
+            while (s2.hasNext()) {
+                String s = s2.next();
+                list.add(s);
             }
         }
-        // Sort the list in ascending alphabetical order
-        Collections.sort(list);
+        Arrays.sort(list, new MyComparator());
 
-        // Display the list
-        System.out.println(list);
+    }
+    public static class MyComparator implements Comparator<String> {
+        @Override
+        public int compare(String s1, String s2){
+            return s1.length() - s2.length();
+        }
     }
 }
